3d038408be7f9a451bbb95218364190f095b1a66
stefanjenkins <88261567+stefanjenkins@users.noreply.github.com>
2021-08-06 10:55:20 +1200

    Update WallSquare.java

--- code/WallSquare.java
+++ code/WallSquare.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  * WallSquare is a subclass of Square, representing a Square that GameCharacters
  * can't be placed on.
@@ -11,4 +14,11 @@ public class WallSquare extends Square {
 
   @Override
   public String toString(){ return "X"; }
+  
+  @Override
+  public void drawSquare(Graphics g) {
+	  g.setColor(new Color(36,36,36));
+	  g.fillRect((col*SIZE)+WALL, (row*SIZE)+WALL, SIZE, SIZE);
+	  
+  }
 }
