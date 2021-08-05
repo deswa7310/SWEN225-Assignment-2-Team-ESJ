0e8fc037382e5705e0f1da970b68452cbe748067
stefanjenkins <88261567+stefanjenkins@users.noreply.github.com>
2021-08-06 10:51:56 +1200

    Update GameCharacter.java

--- code/GameCharacter.java
+++ code/GameCharacter.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  * GameCharacter is a subclass of Card and represents a playable character in Murder Madness.
  * They can move onto certain Squares and into Estates.
@@ -36,5 +39,59 @@ public class GameCharacter extends Card {
   /** Directly sets the GameCharacter's square. Used during guesses. */
   public void setSquare(Square square){ this.square = square; }
   public Square getSquare(){ return square; }
-
+  
+  /**
+   * draws character on grid by calling drawCharToken.
+   * @param g
+   */
+  public void drawCharacter(Graphics g) {
+	  int x = 0;
+	  int y = 0;
+	  if(!this.inEstate()) {
+		  x = square.col;
+		  y = square.row;
+		  drawCharToken(g,x,y);
+	  }
+  }
+  
+  /**
+   * Draws the character tokens. Used by drawCharacter as well as drawEstateContents.
+   * @param g
+   * @param x
+   * @param y
+   */
+  public void drawCharToken(Graphics g, int x, int y) {
+	  x = (x * Square.SIZE) + Square.WALL;
+	  y = (y * Square.SIZE) + Square.WALL;
+	  int offset = Square.SIZE/2;
+	  char in = this.initial;
+	  switch(in) {
+  		case 'L':
+  			g.setColor(new Color(42,161,70));
+  			g.fillOval(x, y, Square.SIZE, Square.SIZE);
+  			g.setColor(Color.WHITE);
+  			g.drawString("L",x+offset,y+offset);
+  			break;
+  		case 'B':
+  			g.setColor(new Color(38,75,204));
+  			g.fillOval(x, y, Square.SIZE, Square.SIZE);
+  			g.setColor(Color.WHITE);
+  			g.drawString("B",x+offset,y+offset);
+  			break;
+  		case 'M':
+  			g.setColor(new Color(248,208,52));
+  			g.fillOval(x, y, Square.SIZE, Square.SIZE);
+  			g.setColor(Color.WHITE);
+  			g.drawString("M",x+offset,y+offset);
+  			break;
+  		default:
+  			g.setColor(new Color(228,0,39));
+  			g.fillOval(x, y, Square.SIZE, Square.SIZE);
+  			g.setColor(Color.WHITE);
+  			g.drawString("P",x+offset,y+offset);
+  			break;
+	  }
+  }
+  
+  
 }
